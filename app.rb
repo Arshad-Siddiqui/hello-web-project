943571b831c9c3c9f23048807dd973f91e0c68fd
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-03 14:41:22 +0100

    Add #sort_names

--- app.rb
+++ app.rb
@@ -7,4 +7,31 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  get '/' do
+    # The code here is executed when a request is received,
+    # and need to send a response. 
+
+    # We can simply return a string which
+    # will be used as the response content.
+    # Unless specified, the response status code
+    # will be 200 (OK).
+    return 'Some response data'
+  end  
+
+  get '/hello' do
+    "hello #{params[:name]}"
+  end
+
+  post '/submit' do
+    "Thanks #{params[:name]}, you sent this message: \"#{params[:message]}\""
+  end
+
+  get '/names' do
+    "Julia, Mary, Karim"
+  end
+
+  post '/sort-names' do
+    params[:names].split(',').sort.join(',')
+  end
 end
